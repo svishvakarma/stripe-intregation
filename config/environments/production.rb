a6e432521b9f6629194fb9c29c9d222a556dcd6b
sumit vishvakarma <vishvakarma@bestpeers.com>
2023-07-17 15:18:08 +0530

    hot fixed env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-  config.secret_key_base = ENV['stripe_secret_key']
+  config.secret_key_base = ENV['ebay-env']['stripe_secret_key']
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"

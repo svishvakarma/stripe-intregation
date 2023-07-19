7a81172a074c3c0184c940c3721f68d4dfca6517
sumit vishvakarma <vishvakarma@bestpeers.com>
2023-07-19 12:43:44 +0530

    solved syntax error on webhooks

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -6,13 +6,13 @@ class WebhooksController < ApplicationController
 		payload = request.body.read
 		sig_header = request.env['HTTP_STRIPE_SIGNATURE']
 		event = nil
-
+	
 		begin
 			event = Stripe::Webhook.construct_event(
-				payload, sig_header, Rails.application.credentials[:stripe][:webhook]
+				payload, sig_header, Rails.application.credentials.dig(:stripe, :webhook)
 			)
 		rescue JSON::ParserError => e
-			status 400
+			render json: { message: 'Invalid payload' }, status: 400
 			return
 		rescue Stripe::SignatureVerificationError => e
 			# Invalid signature
@@ -20,16 +20,16 @@ class WebhooksController < ApplicationController
 			p e
 			return
 		end
-
+	
 		# Handle the event
 		case event.type
 		when 'checkout.session.completed'
 			session = event.data.object
-				product = Product.find_by(price: session.amount_total)
-				product.increment!(:sales_count)
-			end
+			product = Product.find_by(price: session.amount_total)
+			product.increment!(:sales_count)
 		end
-
+	
 		render json: { message: 'success' }
 	end
+	
 end
